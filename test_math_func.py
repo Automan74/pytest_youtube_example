0b9ace56eecabf18b894f24b25a0394116e25c50
Automan74 <marcus_sss@hotmail.com>
2020-04-27 15:03:10 +0200

    start

--- test_math_func.py
+++ test_math_func.py
@@ -1,24 +1,24 @@
 from math_func import StudentDB
 import pytest
-db=None
-def setup_module(module):
-     print('--------------setup----------------------')
-     global db
-     db = StudentDB()
-     db.connect('data.json')
 
-def teardown_module(module):
+@pytest.fixture(scope='module')
+def db():
+    print('--------------setup----------------------')
+    db = StudentDB()
+    db.connect('data.json')
+    yield db
     print('--------------teardown--------------------')
     db.close()
 
-def test_scott_data():
+
+def test_scott_data(db):
     scott_data = db.get_data('Scott')
     assert scott_data['id'] == 1
     assert scott_data['name'] == 'Scott'
     assert scott_data['result'] == 'pass'
 
 
-def test_mark_data():
+def test_mark_data(db):
     db = StudentDB()
     db.connect('data.json')
     mark_data = db.get_data('Mark')
